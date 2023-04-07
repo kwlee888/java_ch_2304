566cde53ec6772bd6c0aaff8bdc09aa965304cf2
KIMKH <KIMKH@DESKTOP-841DVRO>
2023-04-08 00:29:15 +0900

    자바 프로젝트

---
+++ Java_KJ_SampleProject/src/java_sample/TestPractice.java
@@ -0,0 +1,12 @@
+package java_sample;
+
+public class TestPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+		
+		
+	}
+
+}
